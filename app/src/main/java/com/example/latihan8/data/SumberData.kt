7388edac1fb5ac3c72425d870399899f4ec0f57d
Dianitsya <dianitasyanandamasta@gmail.com>
2023-11-13 20:17:41 +0700

    menambahkan data source

---
+++ app/src/main/java/com/example/latihan8/data/SumberData.kt
@@ -0,0 +1,13 @@
+package com.example.latihan8.data
+
+import com.example.latihan8.R
+
+object SumberData {
+    val flavors = listOf(
+        R.string.natural,
+        R.string.vanilla,
+        R.string.lemon,
+        R.string.milk,
+        R.string.lychee
+    )
+}
